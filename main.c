730bffefc30f97081e308108fc832d494c5d88bc
JohnnyPP <johnnnyppp@gmail.com>
2015-10-03 16:50:51 +0200

    Initial release

---
+++ main.c
@@ -0,0 +1,25 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 3, 2015
+ *      Author: Kolan
+ */
+
+#define F_CPU 1000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+	DDRB = 0x08;
+
+	while(1)
+	{
+		PORTB = 0x00;
+		_delay_ms(500);
+		PORTB = 0x08;
+		_delay_ms(500);
+	}
+	return 0;
+}
